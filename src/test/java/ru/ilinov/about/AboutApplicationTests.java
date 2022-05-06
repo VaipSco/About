968a0c4cbfa84e3c055972f1c02afefa27f544eb
Viktor <vaiperda@gmail.com>
2022-05-06 14:10:56 +0300

    Init commit

---
+++ src/test/java/ru/ilinov/about/AboutApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilinov.about;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
